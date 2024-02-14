ac25129073b29f28c6d981c2083e396ebe02ee87
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2024-02-14 11:28:48 +0300

    add LinkedList.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 
 const numbersCollection = new NumbersCollection([1, 234, 54, -5, 23, 8]);
@@ -13,4 +14,12 @@ const sorter1 = new Sorter(charactersCollection);
 sorter1.sort();
 console.log(charactersCollection.data);
 
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(-7);
+linkedList.add(12);
+linkedList.add(80);
+const sorter2 = new Sorter(linkedList);
+sorter2.sort();
+linkedList.print();
 
