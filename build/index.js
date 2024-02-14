36d16f3886ebc716f4533b9dc69b2280ba1e7038
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2024-02-14 10:39:10 +0300

    add CharactersCollection.ts

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([1, 234, 54, -5, 23, 8]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('HekliNdo!');
+const sorter1 = new Sorter_1.Sorter(charactersCollection);
+sorter1.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 
 const numbersCollection = new NumbersCollection([1, 234, 54, -5, 23, 8]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('HekliNdo!');
+const sorter1 = new Sorter(charactersCollection);
+sorter1.sort();
+console.log(charactersCollection.data);
+
+
