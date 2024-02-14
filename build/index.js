823d9355e1bd805b855b60868785e82aac7170cf
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2024-02-14 10:18:19 +0300

    implement interface Sortable, add NumbersCollection.ts

--- build/index.js
+++ build/index.js
@@ -1,3 +1,8 @@
 "use strict";
-console.log('hello world');
-const thing = 'thing';
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([1, 234, 54, -5, 23, 8]);
+const sorter = new Sorter_1.Sorter(numbersCollection);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-console.log('hello');
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-const thing = 'thing';
+
+const numbersCollection = new NumbersCollection([1, 234, 54, -5, 23, 8]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(sorter.collection);
