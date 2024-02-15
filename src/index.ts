546ae70c464daa7ce7f9b3fba52590ef661b4d42
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2024-02-15 11:00:11 +0300

    make class Sorter abstract

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,11 @@ import { LinkedList } from "./LinkedList";
 
 
 const numbersCollection = new NumbersCollection([1, 234, 54, -5, 23, 8]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('HekliNdo!');
-const sorter1 = new Sorter(charactersCollection);
-sorter1.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -19,7 +17,6 @@ linkedList.add(100);
 linkedList.add(-7);
 linkedList.add(12);
 linkedList.add(80);
-const sorter2 = new Sorter(linkedList);
-sorter2.sort();
+linkedList.sort();
 linkedList.print();
 
